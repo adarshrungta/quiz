37987d3994ee011e0ff86471ba7fab81e9557d43
adarshrungta <adarsh.meets.com>
2018-04-04 22:01:46 +0530

    adding prev and next functinality

--- src_users/components/App.js
+++ src_users/components/App.js
@@ -17,41 +17,61 @@ export class App extends React.Component {
     this.props.dispatch({ type: "QUIZ_FETCH_LIST" });
   }
 
-  handleClick(id){
+  handleClick(id) {
     this.props.dispatch({ type: "OPTION_SELECTED", data: id })
   }
   // render
   render() {
+
     
+      let unanswered_count = 0;
+      let rightanswered_count = 0;
+      let wronganswered_count = 0;
+      (Object.keys(this.props.quiz)).map((question_data) => {
+        if (this.props.quiz[question_data].status == 'unanswered') {
+          unanswered_count = unanswered_count + 1;
+        }
+        else if (this.props.quiz[question_data].status == 'rightanswered') {
+          rightanswered_count = rightanswered_count + 1;
+        }
+        else {
+          wronganswered_count = wronganswered_count + 1;
+        }
+      })
+    
+
     return (
       <div className="container">
-        { !this.props.summary && <div>
-        <h5>
-          {this.props.quiz &&
-            this.props.quiz[`question_id_${this.props.current_index}`] &&
-            this.props.quiz[`question_id_${this.props.current_index}`].title}
-        </h5>
-        <ul>
-          {this.props.quiz &&
-            this.props.quiz[`question_id_${this.props.current_index}`] &&
-            this.props.quiz[`question_id_${this.props.current_index}`].options &&
-            this.props.quiz[`question_id_${this.props.current_index}`].options.map((item, i) => {
-              return (
-                <li onClick={() => this.handleClick(item.id)} className="list-group-item" key={i}>
-                  {item.text}
-                </li>
-              );
-            })}
-        </ul>
-        <button onClick={() => this.props.dispatch({type : "SKIPPED_QUESTION"})} className="btn btn-primary">Skip</button>
-        
-        
+        {!this.props.summary && <div>
+          <h5>
+            {this.props.quiz &&
+              this.props.quiz[`question_id_${this.props.current_index}`] &&
+              this.props.quiz[`question_id_${this.props.current_index}`].title}
+          </h5>
+          <ul>
+            {this.props.quiz &&
+              this.props.quiz[`question_id_${this.props.current_index}`] &&
+              this.props.quiz[`question_id_${this.props.current_index}`].options &&
+              this.props.quiz[`question_id_${this.props.current_index}`].options.map((item, i) => {
+                return (
+                  <li onClick={() => this.handleClick(item.id)} className="list-group-item" key={i}>
+                    {item.text}
+                  </li>
+                );
+              })}
+          </ul>
+          <button onClick={() => this.props.dispatch({ type: "PREV_QUESTION" })} className="btn btn-primary">Prev</button>
+          <button onClick={() => this.props.dispatch({ type: "NEXT_QUESTION" })} className="btn btn-primary">Next</button>
+
+
         </div>
         }
-        { this.props.summary && <div>
-          Wrong Answer : {this.props.wrong_answer} <br/ >
-          Right Answer : {this.props.right_answer}
-        </div>  }
+        
+        {this.props.summary && <div>
+          Wrong Answer : {wronganswered_count + unanswered_count } <br />
+          Right Answer : {rightanswered_count}
+          
+        </div>}
 
       </div>
     );
@@ -63,8 +83,8 @@ function mapStateToProps(state) {
   return {
     current_index: state.quiz.current_index,
     quiz: state.quiz.quiz_data,
-    right_answer : state.quiz.right_answer_count,
-    wrong_answer : state.quiz.wrong_answer_count,
+    right_answer: state.quiz.right_answer_count,
+    wrong_answer: state.quiz.wrong_answer_count,
     summary: state.quiz.summary,
   };
 }
--- src_users/reducers/quiz.js
+++ src_users/reducers/quiz.js
@@ -1,31 +1,43 @@
-// users reducer
+// quiz reducer
 const INITIAL_STATE = {
   quiz_data : {},
   right_answer_count : 0,
   wrong_answer_count : 0,
   current_index : 1,
   summary: false,
+  //status : []
 }
-
 
 export default function quiz(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'QUIZ_SAVE_LIST':
       //return action.quiz;
+      (Object.keys(action.quiz)).map((question_data) => {
+        action.quiz[question_data].status = 'unanswered';
+      })
       return Object.assign({}, state, {quiz_data: action.quiz});
     case 'OPTION_SELECTED' :
       let right_answer_count = state.right_answer_count;
       let wrong_answer_count = state.wrong_answer_count;
       let current_index = state.current_index;
-      if(state.quiz_data[`question_id_${state.current_index}`].answer_id == action.data)
+      let quiz_data = JSON.parse(JSON.stringify(state.quiz_data))  ;   
+
+      if(state.quiz_data[`question_id_${state.current_index}`].answer_id == action.data){
+        quiz_data[`question_id_${state.current_index}`].status = "rightanswered";
         right_answer_count++;
-      else
+      }
+      else{
         wrong_answer_count++;
+        //status = "wronganswered";
+        quiz_data[`question_id_${state.current_index}`].status = "wronganswered";
+      }
+        
       current_index++;
       if (current_index === Object.keys(state.quiz_data).length + 1 ) {
         return Object.assign({}, state, 
           {
-            right_answer_count: right_answer_count, 
+            quiz_data : quiz_data,    
+            right_answer_count : right_answer_count, 
             wrong_answer_count: wrong_answer_count, 
             current_index : current_index,
             summary: true,
@@ -33,28 +45,28 @@ export default function quiz(state = INITIAL_STATE, action) {
       }
       return Object.assign({}, state, 
         {
+          quiz_data : quiz_data,
           right_answer_count: right_answer_count, 
           wrong_answer_count: wrong_answer_count, 
           current_index : current_index
         });
-     case 'SKIPPED_QUESTION' : 
-      
+        
+     case 'NEXT_QUESTION' : 
+     
       let right_answer_count_1 = state.right_answer_count;
       let wrong_answer_count_1 = state.wrong_answer_count;
       let current_index_1 = state.current_index;
-     
-      if(state.quiz_data[`question_id_${state.current_index}`].answer_id == action.data)
-        right_answer_count++;
-      else
-        wrong_answer_count++;
-      current_index++;
-
+      
+      quiz_data = JSON.parse(JSON.stringify(state.quiz_data));
+      quiz_data[`question_id_${state.current_index}`].status = "unanswered";
       
       wrong_answer_count_1++;
+      //status = "unanswered";
       current_index_1++;
-      if (current_index === Object.keys(state.quiz_data).length + 1)  {
+      if (current_index_1 === Object.keys(state.quiz_data).length + 1)  {
         return Object.assign({}, state, 
         {
+          quiz_data : quiz_data,
           right_answer_count: right_answer_count_1,
           wrong_answer_count: wrong_answer_count_1, 
           current_index : current_index_1,
@@ -63,11 +75,26 @@ export default function quiz(state = INITIAL_STATE, action) {
       }
       return Object.assign({}, state, 
         {
+          quiz_data : quiz_data,
           right_answer_count: right_answer_count_1,
           wrong_answer_count: wrong_answer_count_1, 
           current_index : current_index_1
-        });  
-
+        });
+        
+        case 'PREV_QUESTION' : 
+     
+        let right_answer_count_2 = state.right_answer_count;
+        let wrong_answer_count_2 = state.wrong_answer_count;
+        let current_index_2 = state.current_index;
+        
+        current_index_2--;
+        return Object.assign({}, state, 
+          {
+            right_answer_count: right_answer_count_2,
+            wrong_answer_count: wrong_answer_count_2, 
+            current_index : current_index_2
+          });
+        
     // initial state
     default:
       return state;
